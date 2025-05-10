56e192608b6fec7ce2bd0f08fb40cf1565376924
Looman <looman431@gmail.com>
2025-05-10 22:28:33 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+    esbuild: {
+        jsx: 'automatic',
+    },
+    resolve: {
+        alias: {
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+        },
+    },
+    server: { // Секция server на нужном уровне
+        host: true, // Слушать на всех IP
+        port: 5173, // Порт Vite
+        hmr: {
+            host: '192.168.0.182', // <--- Указываем публичный IP! Замени, если IP другой.
+        },
+        cors: true,
+    },
+});
