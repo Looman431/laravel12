a3e99ee046d812e97f18ca9d100478347ae87e32
Nikita Sharaphanenko <looman431@gmail.com>
2025-05-12 08:21:49 +0000

    белую тему ставим

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
+            html {
+                background-color: oklch(1 0 0);
+            }
+
             html.dark {
                 background-color: oklch(0.145 0 0);
             }
         </style>
         <script>
-            document.documentElement.classList.add('dark');
+            document.documentElement.classList.add('');
         </script>
         @routes
         @viteReactRefresh
