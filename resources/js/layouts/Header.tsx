d44dfd1ea539f726c9bda43bd571d7c80c407a22
Atstion <131211an@gmail.com>
2025-05-12 13:30:14 +0500

    Тыкаю кнопку день 1

--- resources/js/layouts/Header.tsx
+++ resources/js/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@inertiajs/react';
 
 export default function Header({children}: { children: React.ReactNode }){
     return (
@@ -9,22 +10,20 @@ export default function Header({children}: { children: React.ReactNode }){
                 flex justify-center items-center
                 rounded-b-xl
                 space-x-7">
-                    <button className="
+                    <Link className="
                     h-10 w-25
                     bg-linear-to-tr from-cyan-500 to-blue-500 rounded-md text-black text-sm font-bold
-                    transform transition-all hover:text-white hover:cursor-pointer">
+                    transform transition-all hover:text-white hover:cursor-pointer" href="">
                         Профиль
-                    </button>
+                    </Link>
 
-                    <button className="
+                    <Link className="
                     h-10 w-25
                     mr-10
                     bg-linear-to-br from-blue-500 to-cyan-500 rounded-md text-black text-sm font-bold
-                    transform transition-all hover:text-white hover:cursor-pointer">
+                    transform transition-all hover:text-white hover:cursor-pointer" href="/projects">
                         Проекты
-                    </button>
-
-                    <button>Test</button>
+                    </Link>
                 </nav>
             </header>
             <main>
