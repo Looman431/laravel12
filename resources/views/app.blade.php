e4c615ba0fc305db52b7095ff64e3ac8a5b7a5d1
Nikita Sharaphanenko <looman431@gmail.com>
2025-05-12 08:20:13 +0000

    дарк тему ставим

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            html.dark {
+                background-color: oklch(0.145 0 0);
+            }
+        </style>
+        <script>
+            document.documentElement.classList.add('dark');
+        </script>
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
