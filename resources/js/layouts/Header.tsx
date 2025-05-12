e2858fdb0a8f3a9c4a174229f03c98da72511a50
Atstion <131211an@gmail.com>
2025-05-12 12:12:48 +0500

    Тесты

--- resources/js/layouts/Header.tsx
+++ resources/js/layouts/Header.tsx
@@ -4,8 +4,13 @@ export default function Header({children}: { children: React.ReactNode }){
     return (
         <>
             <header>
-                <nav>
-                    <h1 className="h-15 bg-neutral-800">dww</h1>
+                <nav className="h-15 bg-neutral-900 items-center">
+                    <button className="
+                    h-7 w-15
+                    bg-linear-to-tr from-cyan-500 to-blue-500 rounded text-black
+                    transform transition-all hover:bg-black hover:text-white">
+                        Профиль
+                    </button>
                 </nav>
             </header>
             <main>
