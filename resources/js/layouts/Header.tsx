4323007157a7c3bc388e0c7f670db82691d4fa29
Atstion <131211an@gmail.com>
2025-05-12 12:27:35 +0500

    Тыкаю кнопку день 1

--- resources/js/layouts/Header.tsx
+++ resources/js/layouts/Header.tsx
@@ -4,10 +4,10 @@ export default function Header({children}: { children: React.ReactNode }){
     return (
         <>
             <header>
-                <nav className="h-15 bg-neutral-900 align-middle flex justify-center">
+                <nav className="h-15 bg-neutral-900 flex justify-center items-center">
                     <button className="
-                    h-9 w-17
-                    bg-linear-to-tr from-cyan-500 to-blue-500 rounded text-black text-sm
+                    h-10 w-20
+                    bg-linear-to-tr from-cyan-500 to-blue-500 rounded text-black text-sm font-bold
                     transform transition-all hover:bg-black hover:text-white">
                         Профиль
                     </button>
