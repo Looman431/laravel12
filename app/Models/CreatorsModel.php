2c748f6f66a13f5f96abd537c4c14e5287f3f09c
Nikita Sharaphanenko <looman431@gmail.com>
2025-05-12 08:58:55 +0000

    Первый коммит

--- app/Models/CreatorsModel.php
+++ app/Models/CreatorsModel.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CreatorsModel extends Model
 {
-    //
+    use HasFactory;
 }
