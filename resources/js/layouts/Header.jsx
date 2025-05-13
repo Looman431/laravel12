753c03b05cdae38feee4088c3a2e6148528b63c1
Atstion <131211an@gmail.com>
2025-05-13 11:03:20 +0500

    Пофиксил вот это вот

--- resources/js/layouts/Header.jsx
+++ resources/js/layouts/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
 
-export default function Header({children}: { children: React.ReactNode }){
+export default function Header({children}){
     return (
         <>
             <header>
