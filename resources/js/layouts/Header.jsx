b4cead6f14cb7c785d2591c7e2781b7f9b91b81c
Atstion <131211an@gmail.com>
2025-05-13 12:48:36 +0500

    Эксперименты день 1

--- resources/js/layouts/Header.jsx
+++ resources/js/layouts/Header.jsx
@@ -5,27 +5,29 @@ export default function Header({children}){
     return (
         <>
             <header>
-                <nav className="h-25
+                <nav className="
+                h-16
                 bg-neutral-900
                 flex justify-center items-center
                 rounded-b-xl
                 space-x-7">
                     <Link className="
-                    h-14 w-32
+                    h-8 w-38
+                    rounded-lg
                     flex items-center content-center justify-center
-                    bg-neutral-950/50 rounded-md text-white text-lg font-semibold
+                    text-white text-md font-semibold
                     border-b-4 border-black/70
-                    transform transition-all hover:text-cyan-100 hover:cursor-pointer hover:opacity-5" href="">
+                    transform transition-all hover:text-cyan-100 hover:cursor-pointer" href="">
                         Профиль
                     </Link>
 
                     <Link className="
-                    h-14 w-32
+                    h-8 w-38
                     mr-10
+                    rounded-lg
                     flex items-center content-center justify-center
-                    bg-neutral-950/50 rounded-md text-white text-lg font-semibold
-                    border-b-4 border-black/70
-                    transform transition-all hover:text-cyan-100 hover:cursor-pointer hover:opacity-5" href="/projects">
+                    text-white text-md font-semibold
+                    transform transition-all hover:text-cyan-100 hover:cursor-pointer hover:" href="/projects">
                         Проекты
                     </Link>
                 </nav>
