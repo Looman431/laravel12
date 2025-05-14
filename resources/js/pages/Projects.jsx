14df711458ab14ae37a06ffe4b8cdd0f4c455f5e
Atstion <131211an@gmail.com>
2025-05-14 12:17:45 +0500

    Эксперименты день 2

--- resources/js/pages/Projects.jsx
+++ resources/js/pages/Projects.jsx
@@ -1,5 +1,6 @@
-import Header from "../layouts/Header.jsx"
+import Header from "../layouts/Header.jsx";
 import React from 'react';
+import { Link } from "@inertiajs/react";
 
 function Projects({CreatorsModel}){
     return(
@@ -8,10 +9,19 @@ function Projects({CreatorsModel}){
             <br/>
             <h1>{CreatorsModel.ProjectName}</h1>
             <h1>{CreatorsModel.ProjectShortDescription}</h1>
+
+            <Link className="
+            h-14 w-28
+            ml-10
+            rounded
+            bg-neutral-950
+            border-b-6 border-black" href="/projects/create">
+                Создать проект
+            </Link>
         </>
         )
 }
 
 Projects.layout = page => <Header children={page}/>
 
-export default Projects
+export default Projects;
