a02a0e159e70018abe7345672977c68094bbc7c3
Looman431 <looman431@gmail.com>
2025-05-15 16:06:17 +0300

    Фронт чиним

--- resources/js/pages/Lore.jsx
+++ resources/js/pages/Lore.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
+import Header from '@/layouts/Header.jsx';
+import Projects from '@/pages/Projects.jsx';
+
 function Lore(){
     return(
         <>
-
+            <h1 className="title">Testik</h1>
         </>
         )
 }
 
-export default
+Lore.layout = page => <Header children={page}/>
+
+export default Lore;
