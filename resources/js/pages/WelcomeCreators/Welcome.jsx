5ce3fd370c542f5803654f9aee8e190b3202c9e2
Atstion <131211an@gmail.com>
2025-05-16 18:18:20 +0500

    Эксперименты день 4

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 export default function Welcome(){
     return (
         <>
-            <img src="/image_2025-05-15_15-05-58%201.png" className="" alt="хуясе"></img>
             <Header children={undefined} />
+            <img src="/image_2025-05-15_15-05-58%201.png" className="w-full" alt="хуясе"></img>
         </>
     );
 }
