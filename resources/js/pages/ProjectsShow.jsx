d70e126d8d75d7ec49cfb0e606512660ae38cc81
Looman431 <looman431@gmail.com>
2025-05-16 16:35:40 +0300

    Не работает отображение, логируем пропсы

--- resources/js/pages/ProjectsShow.jsx
+++ resources/js/pages/ProjectsShow.jsx
@@ -2,6 +2,12 @@ import Header from "../layouts/Header.jsx";
 import React from 'react';
 import { Link } from "@inertiajs/react";
 function ProjectsShow({Projects}){
+    console.log('ProjectsShow компонент загружен'); // Увидим, если компонент начал выполняться
+    console.log('Получен пропс Projects:', Projects);
+    console.log('ID:', Projects ? Projects.id : 'нет ID');
+    console.log('Название:', Projects ? Projects.ProjectName : 'нет Названия');
+    console.log('Описание:', Projects ? Projects.ProjectShortDescription : 'нет Описания');
+    console.log('Путь к изображению:', Projects ? Projects.ProjectImagePath : 'нет Пути');
     return(
         <>
             <h1 className="title">Testttt</h1>
