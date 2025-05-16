42866fd5431dc13e2c7859dda5b8826ca63fc36d
Atstion <131211an@gmail.com>
2025-05-16 19:14:14 +0500

    Эксперименты день 4

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -6,9 +6,9 @@ export default function Welcome(){
     return (
         <>
             <Header children={undefined} />
-            <img src="/image_2025-05-15_15-05-58%201.png" className="w-full relative" alt=""></img>
-            <div className="w-128 h-128 absolute flex justify-center items-center h-screen mt-4">
-                <img src="/2022-07-16_18-40-19-removebg-preview.png" alt=""></img>
+            <div className="relative">
+                <img src="/image_2025-05-15_15-05-58%201.png" className="w-full object-cover" alt=""></img>
+                <img src="/2022-07-16_18-40-19-removebg-preview.png" className="absolute mx-auto" alt=""></img>
             </div>
         </>
     );
