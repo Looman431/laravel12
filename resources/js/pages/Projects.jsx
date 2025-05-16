0ff22fdb2146683b8c936bb40d78e53a4a19818c
Looman431 <looman431@gmail.com>
2025-05-16 15:57:59 +0300

    Допил

--- resources/js/pages/Projects.jsx
+++ resources/js/pages/Projects.jsx
@@ -38,6 +38,25 @@ function Projects({Project}){
                     </div>
                 ))}
             </div>
+
+            <div className="py-12 px-4">
+                {Project.links.map(link => (
+                    link.url ? (
+                        <Link
+                            key={link.label}
+                            href={link.url}
+                            dangerouslySetInnerHTML={{__html: link.label }}
+                            className={p-1 mx-1 ${ link.active ? "text-blue-500 font-bold" : ""}}
+                        />
+                    ) : (
+                        <span
+                            key={link.label}
+                            dangerouslySetInnerHTML={{__html: link.label }}
+                            className="p-1 mx-1 text-slate-300"
+                        ></span>
+                    )
+                ))}
+            </div>
         </>
         )
 }
