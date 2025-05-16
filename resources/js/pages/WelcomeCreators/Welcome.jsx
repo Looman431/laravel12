88fd3f0e0907c7009058fa6720f31622ced0e67d
Atstion <131211an@gmail.com>
2025-05-16 18:26:48 +0500

    Эксперименты день 4

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -6,8 +6,8 @@ export default function Welcome(){
     return (
         <>
             <Header children={undefined} />
-            <img src="/image_2025-05-15_15-05-58%201.png" className="w-full" alt=""></img>
-            <img src="/blue-shield-armor-empty-plate-vector_-fotor-bg-remover-2024011621435.png" className="w-256 h-256 absolute" alt=""></img>
+            <img src="/image_2025-05-15_15-05-58%201.png" className="w-full z-1" alt=""></img>
+            <img src="/2022-07-16_18-40-19-removebg-preview.png" className="w-128 h-128 z-2" alt=""></img>
         </>
     );
 }
