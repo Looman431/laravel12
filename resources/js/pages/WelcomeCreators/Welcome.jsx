2afe26f013ac61a40523a18054129da99678632b
Atstion <131211an@gmail.com>
2025-05-31 22:31:08 +0500

    Эксперименты день 5

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -10,6 +10,7 @@ export default function Welcome(){
                 <img src="/image_2025-05-15_15-05-58%201.png" className="w-full object-cover" alt=""></img>
                 <img src="/2022-07-16_18-40-19-removebg-preview.png" className="absolute mx-auto" alt=""></img>
             </div>
+            <h1>Креаторс</h1>
         </>
     );
 }
