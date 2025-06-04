bf1ce1881496c0bbd6d6b17d530c3926a5004114
Atstion <131211an@gmail.com>
2025-06-04 17:23:42 +0500

    - Делаю фон велкома

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -9,7 +9,6 @@ export default function Welcome(){
             <div className="flex justify-center">
                 <img src="/site-bg.png" className="
                 w-500 h-200
-                object-cover
                 rounded-2xl
                 mt-10" alt=""></img>
                 <img src="/2022-07-16_18-40-19-removebg-preview.png" className="absolute" alt=""></img>
