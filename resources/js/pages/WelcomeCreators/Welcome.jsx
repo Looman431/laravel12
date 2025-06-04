e5505010f630208f577bcf63ac3b6b43d01f019a
Atstion <131211an@gmail.com>
2025-06-04 17:09:32 +0500

    - Делаю фон велкома

--- resources/js/pages/WelcomeCreators/Welcome.jsx
+++ resources/js/pages/WelcomeCreators/Welcome.jsx
@@ -8,10 +8,10 @@ export default function Welcome(){
             <Header children={undefined} />
             <div className="flex justify-center">
                 <img src="/site-bg.png" className="
-                w-100 h-100
+                w-1760 h-1080
                 object-cover
                 rounded-xl" alt=""></img>
-                <img src="/2022-07-16_18-40-19-removebg-preview.png" className="" alt=""></img>
+                <img src="/2022-07-16_18-40-19-removebg-preview.png" className="absolute" alt=""></img>
                 <h1 className="absolute mx-auto">Креаторс</h1>
             </div>
 
